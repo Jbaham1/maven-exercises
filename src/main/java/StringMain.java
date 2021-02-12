3416639fce9a9e34021e98848d5c8739da0027c1
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-12 14:17:25 -0600

    swapcase

--- src/main/java/StringMain.java
+++ src/main/java/StringMain.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 
 public class StringMain {
+
+    private static StringMain StringUtils;
+
     public static void main(String[] args) {
         System.out.println("Enter a value: ");
         Scanner userInput = new Scanner(System.in);
         String i = userInput.nextLine();
         System.out.println("The value " + i +" is numeric? "+isNumeric(i));
         System.out.println("The value "+ i +" is a string? "+ isAlpha(i));
+        String str2 = StringUtils.swapCase(i);
+        System.out.println(str2);
     }
+
+//    private static String swapCase(String str) {
+//
+//    }
 
     private static boolean isAlpha(String str) {
         // null or empty
