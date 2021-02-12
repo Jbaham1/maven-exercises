33831fe43950468de11f7bf4d4bd763b8d14ca5e
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-12 14:03:29 -0600

    StringMain, isNumeric isAlpha

---
+++ src/main/java/StringMain.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class StringMain {
+    public static void main(String[] args) {
+        System.out.println("Enter a value: ");
+        Scanner userInput = new Scanner(System.in);
+        String i = userInput.nextLine();
+        System.out.println("The value " + i +" is numeric? "+isNumeric(i));
+        System.out.println("The value "+ i +" is a string? "+ isAlpha(i));
+    }
+
+    private static boolean isAlpha(String str) {
+        // null or empty
+        if (str == null || str.length() == 0) {
+            return false;
+        }
+
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isNumeric(final String str) {
+
+        // null or empty
+        if (str == null || str.length() == 0) {
+            return false;
+        }
+
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+
+}
